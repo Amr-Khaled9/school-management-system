f3041698423d875dff2eec2697a33fc127f103c4
Amr-khaled9 <amrkhaled500@gmail.com>
2025-08-02 16:48:23 +0300

    Localization

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,38 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Mcamara\LaravelLocalization\Facades\LaravelLocalization;
+
+
+
+Route::group(
+    [
+        'prefix' => LaravelLocalization::setLocale(),
+        'middleware' => ['localeSessionRedirect', 'localizationRedirect', 'localeViewPath']
+    ],
+    function () {
+        Route::get('/', function () {
+            return view('dashboard'); 
+        });
+    }
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('dashboard');
 });
